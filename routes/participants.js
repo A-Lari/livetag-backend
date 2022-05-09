ead1e68d0686f45bc6cb88b3eaef5973b533b02a
greg-3166 <gregory.gallet@waygo-consulting.com>
2022-05-09 13:38:49 +0200

    Ajoute de la liste des participants

--- routes/participants.js
+++ routes/participants.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+const Participant = require("../models/Participants");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  Participant.find()
+    .populate(["role", "optional_activities", "event"])
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
