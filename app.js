c978d466f4cd4dd813bb3f6f0a2966b0735043fc
greg-3166 <gregory.gallet@waygo-consulting.com>
2022-05-09 14:59:55 +0200

    Ajout populate dans liste participants

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
 const participantsRouter = require("./routes/participants");
 const authRouter = require("./routes/auth");
 
--- routes/participants.js
+++ routes/participants.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 const Participant = require("../models/Participants");
+const Role = require("../models/Roles");
+const Event = require("../models/Events");
+const Activities = require("../models/Activities");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
