40b55c0739d32e6e13f43929923ee261209ccb7e
A-Lari <lari.anthony84@gmail.com>
2022-05-09 16:01:30 +0200

    activities create

--- routes/activities.js
+++ routes/activities.js
@@ -1,29 +1,47 @@
 var express = require("express");
 var router = express.Router();
-const ActivityModel = require("../models/Activities");
+const ActivitiesModel = require("../models/Activities");
+const { checkAuth } = require("./checkAuth");
 
-/* GET activities page. */
-router.get("/", function (req, res) {
-  ActivityModel.find().then((result) => {
+/* GET activities page and create*/
+router.get("/", checkAuth, function (req, res) {
+  ActivitiesModel.find().then((result) => {
     console.log(result);
     res.send(result);
   });
 });
 
+/*create activity*/
+router.post("/", checkAuth, function (req, res) {
+  const { activity_name, activity_date, description, price, event } = req.body;
+
+  ActivitiesModel.create({
+    activity_name,
+    activity_date,
+    description,
+    price,
+    // event: req.event.id,
+  })
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(() => res.sendStatus(500));
+});
+
 /* GET activities par ID */
-router.get("/:id", function (req, res) {
-  ActivityModel.findById(req.params.id).then((result) => {
+router.get("/:id", checkAuth, function (req, res) {
+  ActivitiesModel.findById(req.params.id).then((result) => {
     console.log(result);
     res.send(result);
   });
 });
 
 /* delete activities */
-router.delete("/:id", function (req, res) {
-  console.log("==========> maj activities");
+router.delete("/:id", checkAuth, function (req, res) {
+  console.log("==========> maj activity");
   const idActivity = req.params.id;
   console.log(idActivity);
-  ActivityModel.findByIdAndRemove(idActivity).then((result) => {
+  ActivitiesModel.findByIdAndRemove(idActivity).then((result) => {
     console.log("activité supprimé", result);
     res.send("activité supprimé");
   });
