f526fcb0e6a84e5d9d4cbdd21b218b27ace656d3
Christian ly <christian_ly@outlook.fr>
2022-05-09 09:38:25 +0200

    Initialisation backend + ajout auth

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var express = require('express');
+require("dotenv").config();
+require("./db");
+const cors = require("cors");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require("./routes/auth");
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use("/auth", authRouter);
 app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
