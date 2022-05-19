a7d6c842c2d5eac672e814a9168780f3c9256d52
greg-3166 <gregory.gallet@waygo-consulting.com>
2022-05-19 17:02:34 +0200

    Bug fonction putUserPassword

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,4 +1,7 @@
 const { UserModel } = require("../models/Users");
+const bcrypt = require("bcrypt");
+
+const salts = 10;
 
 const users = {
   // Utilisateur Courtant
@@ -15,16 +18,29 @@ const users = {
     if (!organisation) return res.sendStatus(400);
     if (!email) return res.sendStatus(400);
 
-    console.log(email);
-    //await UserModel.findByIdAndUpdate(req.user._id)
+    UserModel.findByIdAndUpdate(req.user._id, {
+      $set: { organisation, email },
+    }).then((result) => {
+      res.send("Utilisateur modifié");
+    });
   },
 
-  putUserPassword(req, res) {
+  async putUserPassword(req, res) {
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
     if (!password) return res.sendStatus(400);
     if (!confirmPassword) return res.sendStatus(400);
     // password = confirmPassword ?
     if (password !== confirmPassword) return res.sendStatus(400);
-    console.log(password);
+
+    const hashedPassword = await bcrypt.hash(password, salts);
+
+    UserModel.findByIdAndUpdate(req.user._id, {
+      $set: { password: hashedPassword },
+    }).then((result) => {
+      res.send("Utilisateur modifié");
+    });
   },
 };
 
